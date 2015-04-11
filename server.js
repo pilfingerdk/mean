69b58a093bd8e43de8e5a9b2b3ddb22f18af14bc
Martin Toft <pilfinger.toft@gmail.com>
2015-04-11 16:08:09 +0200

    Played with mongoose / load save from db

--- server.js
+++ server.js
@@ -11,7 +11,39 @@ var methodOverride = require('method-override');
 var db = require('./config/db');
 
 var port = process.env.PORT || 8080; // set our port
- mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
+ 
+var dbCon = mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
+
+var kittySchema = mongoose.Schema({
+    name: String
+});
+
+var Kitten = mongoose.model('Kitten', kittySchema);
+
+var silence = new Kitten({ name: 'Mis' });
+console.log(silence.name); // 'Silence'
+
+//dbCon.on('error', console.error.bind(console, 'connection error:'));
+
+/*dbCon.once('open', function (callback) {
+  //console.dir(dbCon);
+  
+
+    
+});*/
+
+/*silence.save(function (err, silence) {
+      if (err) return console.error(err);
+
+});*/
+
+Kitten.find(function (err, kittens) {
+  if (err) return console.error(err);
+  console.log(kittens)
+})
+
+
+  
 
 // get all data/stuff of the body (POST) parameters
 app.use(bodyParser.json()); // parse application/json 
