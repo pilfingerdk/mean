a7d814dc381cc4c18ee38b4c1532b9576dbba87e
Martin Toft <pilfinger.toft@gmail.com>
2015-04-14 02:41:44 +0200

    Played with server.js to learn more about mongoose. Nothing big here. Title of Index changed too

--- server.js
+++ server.js
@@ -8,23 +8,60 @@ var methodOverride = require('method-override');
 // configuration ===========================================
 	
 // config files
-var db = require('./config/db');
-
+var db = require('./config/db'); // includes my mongoDB-connection for my DB-provider
 var port = process.env.PORT || 8080; // set our port
- 
-var dbCon = mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
+var dbCon = mongoose.connect(db.url); // connect to our mongoDB database 
 
-var kittySchema = mongoose.Schema({
-    name: String
+
+
+// grab the things we need (We don't need it actually..Don't know why yet.)
+/*var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+*/
+
+// create a schema
+/* For later us.. to see types and nested data
+
+  var userSchema = new Schema({
+  name: String,
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  admin: Boolean,
+  location: String,
+  meta: {
+    age: Number,
+    website: String
+  },
+  created_at: Date,
+  updated_at: Date
 });
 
-var Kitten = mongoose.model('Kitten', kittySchema);
 
-var silence = new Kitten({ name: 'Mis' });
-console.log(silence.name); // 'Silence'
+// the schema is useless so far
+// we need to create a model using it
+var User = mongoose.model('User', userSchema);
+
+// make this available to our users in our Node applications
+module.exports = User;
+*/
+
+
+// create a schema
+
+// Creating new Objects for the DB
+/*var shoe = mongoose.Schema({
+    name: String;
+	size: String;
+});
+*/
+//var Kitten = mongoose.model('Kitten', kittySchema);
+
+//var silence = new Kitten({ name: 'Mis' });
+//console.log(silence.name); // 'Silence'
 
 //dbCon.on('error', console.error.bind(console, 'connection error:'));
 
+// Testing dbCon.once -> status: it failed.
 /*dbCon.once('open', function (callback) {
   //console.dir(dbCon);
   
@@ -37,10 +74,12 @@ console.log(silence.name); // 'Silence'
 
 });*/
 
+/*
 Kitten.find(function (err, kittens) {
   if (err) return console.error(err);
   console.log(kittens)
 })
+/*
 
 
   
@@ -58,5 +97,5 @@ require('./app/routes')(app); // pass our application into our routes
 
 // start app ===============================================
 app.listen(port);	
-console.log('Toft\'\s App is working on port ' + port); 		// shoutout to the user
-exports = module.exports = app; 			// expose app
+console.log('Toft\'\s App is working on port ' + port);     // shoutout to the user
+exports = module.exports = app;                             // expose app 
